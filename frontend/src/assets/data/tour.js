4ae40c335e82b490ecb9e2295076f2da238784b4
Md. Iftekher Haider Rakib <125182620+Ihrakib@users.noreply.github.com>
2025-11-27 13:04:36 +0600

    Create tour.js with tour data and images
    Added tour data including titles, locations, and images.

---
+++ frontend/src/assets/data/tour.js
@@ -0,0 +1,143 @@
+import tourImg01 from "../images/tour-img01.jpg";
+import tourImg02 from "../images/tour-img02.jpg";
+import tourImg03 from "../images/tour-img03.jpg";
+import tourImg04 from "../images/tour-img04.jpg";
+import tourImg05 from "../images/tour-img05.jpg";
+import tourImg06 from "../images/tour-img06.jpg";
+import tourImg07 from "../images/tour-img07.jpg";
+
+const tours = [
+
+  {
+    "title": "Ratargul Swamp Forest,Sylhet",
+    "city": "Sylhet",
+    "address": "Gowainghat, Sylhet",
+    "distance": 300,
+    "price": 100,
+    "maxGroupSize": 10,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img01.jpg",
+    "featured": true
+  },
+  {
+    "title": "Sadapathor, Sylhet",
+    "city": "Sylhet",
+    "address": "Bholagong in Sylhet",
+    "distance": 300,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img02.jpg",
+    "featured": true
+  },
+  {
+    "title": "Piyain River, Jaflong, Sylhet",
+    "city": "Sylhet",
+    "address": " Gowainghat, Sylhet",
+    "distance": 300,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img03.jpg",
+    "featured": true
+  },
+  {
+    "title": "Lalakhal, Sylhet",
+    "city": "Sylhet",
+    "address": "Jaintiapur, Sylhet",
+    "distance": 300,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img04.jpg",
+    "featured": true
+  },
+  {
+    "title": "Kaptai Lake, Chittagong",
+    "city": "Chittagong",
+    "address": " Kaptai,Rangamati, Chittagong",
+    "distance": 400,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img05.jpg",
+    "featured": true
+  },
+  {
+    "title": "Khoiyachara Waterfalls",
+    "city": "Chittagong",
+    "address": " Mirsharai, Chittagong,",
+    "distance": 400,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img06.jpg",
+    "featured": true
+  },
+  {
+    "title": "Sajek Valley, Rangamati",
+    "city": "Chittagong",
+    "address": "Baghaichhari,Rangamati",
+    "distance": 400,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img07.jpg",
+    "featured": true
+  },
+  {
+    "title": "Cox's Bazar Sea Beach",
+    "city": "Chittagong",
+    "address": "Cox's Bazar Sadar, Cox's Bazar",
+    "distance": 400,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img08.jpg",
+    "featured": true
+  },
+  {
+    "title": "Jaflong, Sylhet",
+    "city": "Sylhet",
+    "address": "Somewhere in Sylhet",
+    "distance": 300,
+    "price": 99,
+    "maxGroupSize": 5,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img03.jpg",
+    "featured": false
+  },
+  {
+    "title": "Tanguar Haor, Sunamganj",
+    "city": "Sylhet",
+    "address": "Tahirpur, Sunamganj",
+    "distance": 300,
+    "price": 99,
+    "maxGroupSize": 8,
+    "desc": "this is the description",
+    "reviews": [],
+
+    "photo": "/tour-images/tour-img09.jpg",
+    "featured": false
+  }
+];
+
+export default tours;
